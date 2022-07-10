59f5c1bd289a736b23c4b49513b514f5ccfb3883
loubarb <louis.barbati@gmail.com>
2022-07-09 23:46:38 -0400

    Add style to shrink long number input in display and round decimal outputs

--- script.js
+++ script.js
@@ -5,35 +5,65 @@ let displayArea = []
 
 buttonArea.forEach(button => {
   button.addEventListener('click', () => {
+
     if (button.classList.contains('num') || (button.classList.contains('operator'))) {
       displayArea.push(button.value)
       const equationInput = displayArea.join('')
       inputArea.textContent = equationInput
-    }
-
+    } 
+    
     const equationInput = displayArea.join('')
 
     if (button.value === 'C') {
-      displayArea = ['']
+      displayArea = []
       inputArea.textContent = 0
+      inputArea.style.fontSize = ''
     }
 
     if (button.classList.contains('special')) {
       const exp = equationInput ** equationInput
-      inputArea.textContent = exp
+      const round = Number(exp.toFixed(2))
+
+      if (exp !== Math.floor(exp)) {
+        inputArea.textContent = round
+      } else {
+        inputArea.textContent = exp
+      }
     }
 
     if (button.id === 'evaluate') {
       const answer = eval(equationInput)
-      inputArea.textContent = answer
+      const round = Number(answer.toFixed(9))
+
+      if (round.toString().length > 11 || answer.toString().length > 11) {
+        inputArea.style.fontSize = '2.75rem'
+      }
+
+      if (answer !== Math.floor(answer)) {
+        inputArea.textContent = round
+      } else {
+        inputArea.textContent = answer
+      }
     }
 
     if (button.id === 'back') {
       displayArea.pop()
       let backup = displayArea.join('')
       inputArea.textContent = backup
-      
-
     }
+
+    if (displayArea.length === 12) {
+      inputArea.style.fontSize = '2.75rem'
+    } else if (displayArea.length === 13) {
+      inputArea.style.fontSize = '2.5rem'
+    } else if (displayArea.length === 14) {
+      inputArea.style.fontSize = '2.25rem'
+    } else if (displayArea.length === 15) {
+      inputArea.style.fontSize = '2rem'
+    } else if (displayArea.length > 16) {
+      return
+    }
+
   })
 })
+
